716fa7c555823a3fb713a459db3d0602e0cd16da
李佳 <1224325327@qq.com>
2015-10-21 21:53:14 +0800

    t(O(lgn)) 用的是除法，防止越界。然后需要注意的地方就是当输入2的时候循环mid = 0不能循环。 然后在discuss里面了解到了Newton's search。有点厉害啊。

---
+++ Sqrt/Sqrt/main.cpp
@@ -0,0 +1,70 @@
+//
+//  main.cpp
+//  Sqrt
+//
+//  Created by 李佳 on 15/10/21.
+//  Copyright © 2015年 LiJia. All rights reserved.
+//
+
+#include <iostream>
+#include <cmath>
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x <= 0)
+            return 0;
+        if (x == -0)
+            return -0;
+        
+        int start = 0;
+        int end = x / 2;
+        int mid = std::max((start + end) / 2, 1);
+        
+        while(start < end && mid)
+        {
+            int minValue = x / mid;
+            if (minValue == mid)
+                return mid;
+            if (minValue < mid)
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+            mid = (start + end) / 2;
+        }
+        if (mid * mid > x) mid--;
+        return mid;
+    }
+    
+    
+    /*
+     
+     long r = x;
+     while (r * r > x)
+     r = (r + x / r) / 2;
+     return r;
+     
+     */
+};
+
+
+int main(int argc, char* argv[])
+{
+    Solution* pSol = new Solution;
+    while (argc-- > 1)
+    {
+        int value = atoi(*++argv);
+        printf("Sqrt(%d) = %d\n", value, pSol->mySqrt(value));
+    }
+    printf("pow(2, 31) = %f", pow(2, 31));
+    printf("pow(2, 31) = %f", pow(2, 15.5));
+    printf("Sqrt(%d) = %d\n", 100, pSol->mySqrt(2));
+    printf("Sqrt(%d) = %d\n", 100, pSol->mySqrt(2147483647));
+    
+    delete pSol;
+    return 0;
+}
